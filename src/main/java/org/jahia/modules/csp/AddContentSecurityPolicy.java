b5ebc1325920d3eeb434be2e86e8ffee538ca826
Florent BOURASSE <fbourasse@jahia.com>
2019-04-17 13:00:22 +0200

    Possibility to report only the CSP violations.

--- src/main/java/org/jahia/modules/csp/AddContentSecurityPolicy.java
+++ src/main/java/org/jahia/modules/csp/AddContentSecurityPolicy.java
@@ -2,7 +2,9 @@ package org.jahia.modules.csp;
 
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang.StringUtils;
+import org.jahia.modules.csp.actions.ReportOnlyAction;
 import org.jahia.services.content.JCRNodeWrapper;
+import org.jahia.services.content.decorator.JCRSiteNode;
 import org.jahia.services.render.RenderContext;
 import org.jahia.services.render.Resource;
 import org.jahia.services.render.filter.AbstractFilter;
@@ -14,13 +16,16 @@ public class AddContentSecurityPolicy extends AbstractFilter implements Applicat
 
     private static final String CSP_SEPARATOR = ";";
     private static final String CSP_PROPERTY = "policy";
+    private static final String CSP_HEADER = "Content-Security-Policy";
+    private static final String CSP_REPORT_ONLY_HEADER = "Content-Security-Policy-Report-Only";
 
     @Override
     public String execute(String previousOut, RenderContext renderContext, Resource resource, RenderChain chain) throws Exception {
         final HttpServletResponse response = renderContext.getResponse();
         final StringBuffer contentSecurityPolicy = new StringBuffer();
 
-        final String siteContentSecurityPolicy = renderContext.getSite().hasProperty(CSP_PROPERTY) ? renderContext.getSite().getProperty(CSP_PROPERTY).getString() : null;
+        final JCRSiteNode site = renderContext.getSite();
+        final String siteContentSecurityPolicy = site.hasProperty(CSP_PROPERTY) ? site.getProperty(CSP_PROPERTY).getString() : null;
 
         if (StringUtils.isNotEmpty(siteContentSecurityPolicy)) {
             contentSecurityPolicy.append(siteContentSecurityPolicy);
@@ -37,7 +42,15 @@ public class AddContentSecurityPolicy extends AbstractFilter implements Applicat
         }
 
         if (contentSecurityPolicy.length() > 0) {
-            response.setHeader("Content-Security-Policy", contentSecurityPolicy.toString());
+            final String cspHeader;
+            if (site.hasProperty(ReportOnlyAction.CSP_REPORT_ONLY) && site.getProperty(ReportOnlyAction.CSP_REPORT_ONLY).getBoolean()) {
+                final String reportUri = resource.getNodePath() + ".contentSecurityPolicyReportOnly.do";
+                contentSecurityPolicy.append(CSP_SEPARATOR).append(" report-uri ").append(reportUri);
+                cspHeader = CSP_REPORT_ONLY_HEADER;
+            } else {
+                cspHeader = CSP_HEADER;
+            }
+            response.setHeader(cspHeader, contentSecurityPolicy.toString());
         }
 
         return previousOut;
---
+++ src/main/java/org/jahia/modules/csp/actions/ReportOnlyAction.java
@@ -0,0 +1,35 @@
+package org.jahia.modules.csp.actions;
+
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.io.IOUtils;
+import org.jahia.bin.Action;
+import org.jahia.bin.ActionResult;
+import org.jahia.services.content.JCRSessionWrapper;
+import org.jahia.services.content.decorator.JCRSiteNode;
+import org.jahia.services.render.RenderContext;
+import org.jahia.services.render.Resource;
+import org.jahia.services.render.URLResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReportOnlyAction extends Action {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReportOnlyAction.class);
+    public static final String CSP_REPORT_ONLY = "cspReportOnly";
+
+    @Override
+    public ActionResult doExecute(HttpServletRequest req, RenderContext renderContext, Resource resource, JCRSessionWrapper session, Map<String, List<String>> parameters, URLResolver urlResolver) throws Exception {
+        if ("application/csp-report".equals(req.getContentType())) {
+            final JCRSiteNode site = renderContext.getSite();
+            if (site.hasProperty(CSP_REPORT_ONLY) && site.getProperty(CSP_REPORT_ONLY).getBoolean()) {
+                final String report = IOUtils.toString(req.getInputStream());
+                LOGGER.warn(report);
+                return ActionResult.OK;
+            }
+        }
+        return ActionResult.BAD_REQUEST;
+    }
+
+}
